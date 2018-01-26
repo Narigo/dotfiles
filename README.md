d0da3bdcf7c4bbb6289892dd34705ff52c0620f5
Joern Bernhardt <jb@campudus.com>
2018-01-26 08:36:22 +0100

    Add dry-run option (-d) and copy-modules
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # dotfiles - Narigo
 
 Setting up my environments in a modular fashion.
+
+## Mac OS X
+
+1. Install Homebrew.
+2. Run `./install.sh` to setup the environment.
--- install.sh
+++ install.sh
@@ -2,15 +2,26 @@
 
 (
 
+set -e
+
 main() {
+  dry_run=`[ "$1" == "-d" ] && echo true || echo false`
+
+  platform=""
+
+  if $dry_run ; then
+    echo "Dry Run!"
+  fi
   select_platform
 
-  echo "Platform is '$platform'."
-
   if [ "$platform" != "" ]; then
+    echo "Platform is '$platform'."
     settings_script="settings-${platform}.sh"
-    install_module "shell"
-    if [ -e "./$settings_script" ]; then
+
+    install_module "shell" "$platform" $dry_run
+    install_module "git" "$platform" $dry_run
+
+    if [ -e "./$settings_script" ] && ! $dry_run; then
       echo "Setting settings from $settings_script"
       . "$settings_script"
     fi
@@ -18,15 +29,73 @@ main() {
 }
 
 install_module() {
-  moduleName=$1;
-  echo "Installing module $moduleName"
+  module_name=$1;
+  platform=$2;
+  dry_run=$3;
 
-  if [ -e "modules/${moduleName}/concat-file.txt" ]; then
-    fileName=`cat "modules/${moduleName}/concat-file.txt"`
-    rm $HOME/$fileName
-    echo "Writing into $HOME/$fileName"
-    cat "modules/$moduleName/$platform/"* >> "$HOME/$fileName"
+  platform_file="modules/${module_name}/file-${platform}"
+  should_copy_files="modules/${module_name}/.copy-files-to-${platform}"
+
+  if [ -e "$platform_file" ]; then
+    echo "Concatenating files from $module_name into $platform_file"
+    file_name=`cat "$platform_file"`
+
+    if $dry_run ; then
+      file_name=".dry_run/$file_name"
+    fi
+
+    # Back up file before overwriting
+    if [ -f $file_name ] && ! $dry_run; then
+      echo "Backing up $file_name into .backups/$file_name"
+      mkdir -p `dirname ".backups/$file_name"`
+      cp "$file_name" ".backups/$file_name"
+    fi
+
+    echo "Making sure directory of $file_name exists"
+    mkdir -p `dirname "$file_name"`
+
+    echo "Writing into $file_name"
+    echo -n "" > "$file_name"
+    if [ -d "modules/$module_name/all" ]; then
+      echo "Writing platform agnostic things into $file_name"
+      cat "modules/$module_name/all/"* >> "$file_name"
+    fi
+    cat "modules/$module_name/$platform/"* >> "$file_name"
+
+  elif [ -e "$should_copy_files" ]; then
+    copy_files_to=`cat "$should_copy_files"`
+
+    if $dry_run ; then
+      copy_files_to=".dry_run/$copy_files_to"
+    fi
+
+    module_dir="modules/$module_name"
+    echo "Copying files from $module_dir to $copy_files_to"
+    if [ -d "$module_dir/all" ]; then
+      echo "Copying platform agnostic files"
+      copy_files "$module_dir/all" "$copy_files_to"
+    fi
+    if [ -d "$module_dir/$platform" ]; then
+      echo "Copying platform specific files"
+      copy_files "$module_dir/$platform" "$copy_files_to"
+    fi
+
+  else
+    echo "Could not find installation file for platform $platform (looked for $platform_file)"
   fi
+}
+
+copy_files() {
+  from_dir="$1"
+  to_dir="$2"
+
+  mkdir -p "$to_dir"
+  for file_name in `ls -1A "$from_dir"`; do
+    from="$from_dir/$file_name"
+    to="$to_dir/$file_name"
+    echo "Copying $from to $to"
+    cp "$from" "$to"
+  done
 }
 
 select_platform() {
@@ -47,6 +116,6 @@ select_platform() {
   done
 }
 
-main
+main $@
 
 )
